967f6d2609729e21a3ce92403973533ceffed931
AyeshaAzam <ayesh76@msn.com>
2021-09-20 10:09:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## What means the #text nodeName : ` these are the empty line breaks that you have unintentionally introduced in your markup.Use children instead of childNodes if you don't want to include text nodes. Whitespace inside elements is considered as text, and text is considered as nodes.`
+
+## Fetch multiple Urls / using axios to fetch multiple urls: `https://www.codegrepper.com/code-examples/javascript/using+axios+to+fetch+multiple+urls`
+
+## Remove duplicate values from JS array [duplicate]: `https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array`
+
+## javascript: how to remove duplicate arrays inside array of arrays : `https://stackoverflow.com/questions/44014799/javascript-how-to-remove-duplicate-arrays-inside-array-of-arrays`
+
+## Remove Duplicates from an Array: `https://www.javascripttutorial.net/array/javascript-remove-duplicates-from-array/`
+
+## How can I reorder/sort a NodeList in JavaScript? `https://stackoverflow.com/questions/4760080/how-can-i-reorder-sort-a-nodelist-in-javascript`
---
+++ indexFromFeeds.js
@@ -0,0 +1,105 @@
+const newsItems2 = document.getElementById("newsItems2");
+
+const XMLDataFromFeed = () => {
+  const one = "https://www.dn.se/rss/";
+  const two = "https://www.dn.se/rss/kultur/";
+  const three = "https://www.dn.se/rss/sport/";
+  const four = "https://www.dn.se/rss/debatt/";
+  const five = "https://feeds.expressen.se/nyheter";
+  const six = "https://feeds.expressen.se/noje";
+  const seven = "https://feeds.expressen.se/motor";
+  const eight = "https://feeds.expressen.se/gt/ledare";
+  const nine = "https://feeds.expressen.se/kvp/kultur";
+  const ten = "https://www.di.se/rss";
+
+  //const response = await axios.get(`${BASE_URL}/todos?_limit=5`);
+  const getAllUrlsData = async () => {
+    try {
+      const res = await Promise.all([
+        axios.get(one),
+        axios.get(two),
+        axios.get(three),
+        axios.get(four),
+        axios.get(five),
+        axios.get(six),
+        axios.get(seven),
+        axios.get(eight),
+        axios.get(nine),
+        axios.get(ten),
+      ]);
+
+      let newsItemList;
+      var newsItemsArr = [];
+
+      res.map((newItem) => {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(newItem.data, "text/xml");
+        newsItemList = xmlDoc
+          .getElementsByTagName("channel")[0]
+          .querySelectorAll("item");
+
+        // Sort and push into new Array
+        var items = newsItemList;
+        for (var i in items) {
+          if (items[i].nodeType == 1) {
+            // get rid of the whitespace text nodes
+            newsItemsArr.push(items[i]);
+          }
+        }
+      });
+
+      newsItemsArr.sort(function (a, b) {
+        let date1 = new Date(a.getElementsByTagName("pubDate")[0].innerHTML);
+        let date2 = new Date(b.getElementsByTagName("pubDate")[0].innerHTML);
+        return date1 == date2 ? 0 : date1 < date2 ? 1 : -1;
+      });
+      console.log(">>>", newsItemsArr);
+
+      // remove the dublicates
+      let uniqueNewsList = [];
+      newsItemsArr.forEach((newsItem) => {
+        var containsInUniqueList = false;
+        for (let i = 0; i < uniqueNewsList.length; i++) {
+          if (
+            uniqueNewsList[i].getElementsByTagName("guid")[0].innerHTML ===
+            newsItem.getElementsByTagName("guid")[0].innerHTML
+          ) {
+            containsInUniqueList = true;
+          }
+        }
+
+        if (uniqueNewsList.length === 0 || !containsInUniqueList) {
+          uniqueNewsList.push(newsItem);
+        }
+      });
+
+      console.log("unique", uniqueNewsList);
+
+      let tenLatestNews = Array.prototype.slice.call(newsItemsArr).slice(0, 10);
+      console.log("tenLatestNews", tenLatestNews);
+
+      tenLatestNews.forEach((item, index) => {
+        let newsLink = document.createElement("a");
+        newsLink.style.display = "inline";
+        // newsLink.style.whiteSpace = "nowrap";
+
+        newsLink.style.textDecoration = "none";
+        newsLink.href = item.querySelector("link").innerHTML;
+        newsLink.target = "_blank";
+        newsLink.innerHTML = item.querySelector("title").innerHTML;
+
+        let para = document.createElement("p");
+        para.innerHTML = `${index + 1}. \xa0\xa0 `;
+        para.appendChild(newsLink);
+
+        newsItems2.appendChild(para);
+      });
+    } catch (error) {
+      console.log("Error fetching data", error);
+    }
+  };
+
+  getAllUrlsData();
+};
+
+XMLDataFromFeed();
